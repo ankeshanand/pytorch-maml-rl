c84705d6e58de416418bdfef812236945d7cb9b2
ankeshanand <ankeshanand1994@gmail.com>
2019-03-20 14:06:04 -0400

    Use proper weight init for the critic

--- maml_rl/policies/critic.py
+++ maml_rl/policies/critic.py
@@ -2,9 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from collections import OrderedDict
+import numpy as np
 
 from maml_rl.policies.policy import weight_init
 
+def init(module, weight_init, bias_init, gain=1):
+    weight_init(module.weight.data, gain=gain)
+    bias_init(module.bias.data)
+    return module
 
 class Critic(nn.Module):
     def __init__(self, input_size, output_size, hidden_sizes=(), nonlinearity=F.tanh):
@@ -15,10 +20,15 @@ class Critic(nn.Module):
         self.nonlinearity = nonlinearity
         self.num_layers = len(hidden_sizes) + 1
 
+        init_ = lambda m: init(m,
+                               nn.init.orthogonal_,
+                               lambda x: nn.init.constant_(x, 0),
+                               np.sqrt(2))
+
         layer_sizes = (input_size,) + hidden_sizes + (output_size,)
         for i in range(1, self.num_layers + 1):
             self.add_module('layer{0}'.format(i),
-                            nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
+                            init_(nn.Linear(layer_sizes[i - 1], layer_sizes[i])))
 
     def update_params(self, loss, step_size=0.5, first_order=False):
         """Apply one step of gradient descent on the loss function `loss`, with
